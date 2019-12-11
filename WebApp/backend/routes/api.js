76d39074e0fc5ab4d190c2b0c4642f7959142fb8
Alex <alexander2.koch@oth-regensburg.de>
2019-12-11 13:57:39 +0100

    fixed untracked files

--- WebApp/backend/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const socketService = require("../services/socket.service")
-
-router.get("/connect/:ip/:port", socketService.connect.bind(socketService))
-router.get("/action/:name/:id", socketService.action.bind(socketService))
-router.get("/enemies", socketService.getAll.bind(socketService))
-
-module.exports = router
